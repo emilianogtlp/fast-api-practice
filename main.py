c16354b2c305c149ca45aa32fc8599cff0798eac
Emiliano <emiliano@gmail.com>
2022-12-19 10:01:57 -0700

    Changes in status codes of path operations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 
 from pydantic import BaseModel
@@ -30,7 +30,7 @@ def root():
 def get_posts():
     return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,10000000)
@@ -38,6 +38,12 @@ def create_posts(post:Post):
     return {"data":post_dict}
 
 @app.get("/posts/{id}") #Path parameter
-def get_post(id:int):
+def get_post(id:int, response: Response):
     post = find_post(id)
+    
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND
+            , detail=f"Post with id {id} was not found")
+    
     return {"post_detail":post}
