5f5ce2ffa88131e9464e61abc7eaebd275eaf85f
Emiliano <emiliano@gmail.com>
2022-12-19 15:36:46 -0700

    Connection to PostgreSQL and new requirements file

--- app/main.py
+++ app/main.py
@@ -5,8 +5,23 @@ from pydantic import BaseModel
 from typing import Optional
 
 from random import randrange
+import time
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres',password='password123',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was succesful.")
+        break
+    except Exception as error:
+        print(error)
+        print("Connection to database failed")
+        time.sleep(3)
 
 my_posts = [{"title":"First Post", "content":"First Content", "published":True, "id":1},{"title":"Second Post", "content":"Second Content", "published":True,"id":2}]
 
@@ -24,7 +39,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True #If the user does not provide a value, de default is True
-    rating: Optional[int] = None #Optional field with a default value of none/null
+    #rating: Optional[int] = None #Optional field with a default value of none/null
 
 
 @app.get("/")
