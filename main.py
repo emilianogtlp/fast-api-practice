1012229ff4f0e40209fd96866b6f0fad6edba524
Emiliano <emiliano@gmail.com>
2022-12-19 10:38:17 -0700

    New PUT operation

--- main.py
+++ main.py
@@ -53,13 +53,28 @@ def get_post(id:int, response: Response):
     
     return {"post_detail":post}
 
+
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
-
     index = find_index_post(id)
 
-    if not index:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist")
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post with id {id} does not exist")
     
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id:int, post:Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post with id {id} does not exist")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {'data':post_dict}
