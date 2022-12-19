a967d796f49204894cc6df87ce4f7d62dd7ca4a2
Emiliano <emiliano@gmail.com>
2022-12-18 19:02:36 -0700

    First draft of a class

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+
 
 @app.get("/")
 def root():
     return {"Hello":"World"}
+
+@app.post("/createposts")
+def create_posts(post:Post):
+    print(post)
+    return post
