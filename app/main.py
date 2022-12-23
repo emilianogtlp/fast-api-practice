412217b4f28c64b1957d8ce1d1cd421e92e3734b
Emiliano <emilianogtlp@gmail.com>
2022-12-23 13:19:19 -0700

    Secured user's password by hashing

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas,utils
 from .database import engine, get_db
 
 
@@ -102,6 +102,11 @@ def update_post(id:int, post:schemas.PostCreate, db: Session = Depends(get_db)):
 
 @app.post("/users",status_code=status.HTTP_201_CREATED,response_model=schemas.ResponseUser)
 def create_user(user:schemas.UserCreate,db: Session = Depends(get_db)):
+    
+    #hash the password from user.password
+    hashed_pwd = utils.hash(user.password)
+    user.password = hashed_pwd
+    
     new_user = models.User(
         **user.dict() # Unpacks the dictionary
         )
